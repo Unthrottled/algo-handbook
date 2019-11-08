a360b7ffbd4c5a63e43dce8e918b2806f9616995
DATBOI <alexsimons9999@gmail.com>
2019-11-07 18:24:16 -0600

    Hello world

---
+++ cStuff/HelloWorld.c
@@ -0,0 +1,6 @@
+//
+// Created by alex on 11/7/19.
+//
+
+#include "HelloWorld.h"
+
---
+++ cStuff/HelloWorld.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 11/7/19.
+//
+
+#ifndef ALGO_HANDBOOK_HELLOWORLD_H
+#define ALGO_HANDBOOK_HELLOWORLD_H
+
+#endif //ALGO_HANDBOOK_HELLOWORLD_H
