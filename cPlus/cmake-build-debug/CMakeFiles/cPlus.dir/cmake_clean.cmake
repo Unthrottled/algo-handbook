639f4697b9cc1acdb44014387389a83ccd9f7bfd
DATBOI <alexsimons9999@gmail.com>
2019-11-08 19:20:26 -0600

    I dunno what I am doing, but I gotta figure it out

---
+++ cPlus/cmake-build-debug/CMakeFiles/cPlus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cPlus.dir/main.cpp.o"
+  "cPlus"
+  "cPlus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
